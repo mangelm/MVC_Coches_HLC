173e0386dbe2412ac4261fe70e08ff97179d45f8
M. Angel Moreno <miguelangelmorenodelgadot@gmail.com>
2024-12-13 09:22:42 +0100

    inicio proyecto

---
+++ coche_MVC/src/test/java/com/hlc/coche_MVC/CocheMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hlc.coche_MVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocheMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
