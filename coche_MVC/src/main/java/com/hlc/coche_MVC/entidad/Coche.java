13f1fbb7c62ad1a578ff67afe37f03ece97ba198
M. Angel Moreno <miguelangelmorenodelgadot@gmail.com>
2024-12-18 19:09:57 +0100

    Entidad Coche creada

---
+++ coche_MVC/src/main/java/com/hlc/coche_MVC/entidad/Coche.java
@@ -0,0 +1,55 @@
+package com.hlc.coche_MVC.entidad;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Coche {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	
+	private Long id;
+	
+	private String Marca;
+	
+	private String Matricula;
+	
+	private String Color;
+	
+	public Long getId(){
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getMarca(){
+		return Marca;
+	}
+	
+	public void setMarca(String Marca) {
+		this.Marca = Marca;
+	}
+	
+	public String getMatricula(){
+		return Matricula;
+	}
+	
+	public void setMatricula(String Matricula) {
+		this.Matricula = Matricula;
+	}
+	
+	public String getColor(){
+		return Color;
+	}
+	
+	public void setColor(String Color) {
+		this.Color = Color;
+	}
+	
+	
+}
---
+++ coche_MVC/src/main/java/como/hlc/coche_MVC/controlador/CocheController.java
@@ -0,0 +1,8 @@
+package como.hlc.coche_MVC.controlador;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CocheController {
+	
+}
